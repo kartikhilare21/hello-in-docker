2ea7875bde08538b34b10601525fdaa2dee83918
kartikhilare21 <139868205+kartikhilare21@users.noreply.github.com>
2024-04-29 21:23:33 +0530

    Create security_groups.tf

---
+++ modules/vpc/security_groups.tf
@@ -0,0 +1,43 @@
+#Security group
+resource "aws_security_group" "security_group" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  tags = {
+    Name        = "pg-security-group-us-east2-${terraform.workspace}"
+    Maintainer  = "Terraform"
+    Environment = "${terraform.workspace}"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_port_443" {
+  security_group_id = aws_security_group.security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_port_22" {
+  security_group_id = aws_security_group.security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_port_80" {
+  security_group_id = aws_security_group.security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+  from_port         = null
+  to_port           = null
+}
